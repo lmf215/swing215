6de49e19cfba0315d1f2f245fbd9047c9f6b8e17
lmf215 <74659833+lmf215@users.noreply.github.com>
2020-11-30 11:56:14 +0800

    Update 113001.继承父类.R.java

--- 113001.继承父类.R.java
+++ 113001.继承父类.R.java
@@ -5,15 +5,14 @@ public class R {
 	static void Shuzu() {
 		String boy_filePath = "img/boy.gif";
 		String girl_filePath = "img/girl.gif";
-		int[][] array2d = { { 1, 2, 3 }, { 4, 5, 6 } };
+		int[][] array2d = { {1,2},{3,4},{5,6}};
 		System.out.println(boy_filePath);
 		System.out.println(girl_filePath);
-		int i,j;
-		for(i=0; i<array2d.length;i++)
-		{
-			for (j=0; j< array2d.length; ++j) {
-				System.out.println(array2d[i][j]);
+		for (int row = 0; row < array2d.length; row++) {
+			for (int column = 0; column < array2d[0].length; column++) {
+				System.out.println(array2d[row][column]+"");
 			}
+			System.out.println();
 		}
 	}
 }
