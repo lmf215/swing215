bc617dce2157d302d1d1ee44fdd851dd213a3aca
lmf215 <74659833+lmf215@users.noreply.github.com>
2020-11-30 11:58:57 +0800

    Create 113002.接口.R.java

---
+++ 113002.接口.R.java
@@ -0,0 +1,18 @@
+package App;
+
+public interface R {
+
+	static void Shuzu() {
+		String boy_filePath = "img/boy.gif";
+		String girl_filePath = "img/girl.gif";
+		int[][] array2d = { {1,2},{3,4},{5,6}};
+		System.out.println(boy_filePath);
+		System.out.println(girl_filePath);
+		for (int row = 0; row < array2d.length; row++) {
+			for (int column = 0; column < array2d[0].length; column++) {
+				System.out.println(array2d[row][column]+"");
+			}
+			System.out.println();
+		}
+	}
+}
